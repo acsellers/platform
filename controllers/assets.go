9f8e2a15c6632790298e57a428155ffc8901596c
Andrew Sellers <andrew.sellers@gradesfirst.com>
2015-01-20 03:12:40 -0600

    Add a asset module to platform/controllers
    Modules are collections of controllers, actions, etc. that are loaded
    onto a subroute or router using the Mount function. This is a module
    that will serve out a folder, making controllers for each folder, and
    setting cache-control directives if you set a MaxAge on the module.

---
+++ controllers/assets.go
@@ -0,0 +1,64 @@
+package controllers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/acsellers/platform/router"
+)
+
+type AssetModule struct {
+	AssetLocation string
+	MaxAge        time.Duration
+	AllowIndex    bool
+}
+
+func (am AssetModule) Load(sr *router.SubRoute) {
+	files, err := ioutil.ReadDir(am.AssetLocation)
+	if err != nil {
+		return
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			sr.Many(AssetController{
+				BaseController: &router.BaseController{},
+				Location:       file.Name(),
+				CtrlPath:       filepath.Join(am.AssetLocation, file.Name()),
+				MaxAge:         am.MaxAge,
+			})
+		} else {
+			fmt.Println(file.Name())
+		}
+	}
+}
+
+type AssetController struct {
+	*router.BaseController
+	Location string
+	CtrlPath string
+	MaxAge   time.Duration
+}
+
+func (ac AssetController) Path() string {
+	return ac.Location
+}
+
+func (ac AssetController) Show() router.Result {
+	fn := filepath.Join(ac.CtrlPath, ac.Params[":"+ac.Location+"id"])
+	_, err := os.Stat(fn)
+	if err != nil {
+		return router.NotFound{}
+	}
+	if ac.MaxAge.Seconds() != 0.0 {
+		ac.Out.Header().Set(
+			"Cache-Control",
+			fmt.Sprintf("max-age=%.f", ac.MaxAge.Seconds()),
+		)
+	}
+	http.ServeFile(ac.Out, ac.Request, fn)
+	return router.NothingResult{}
+}
